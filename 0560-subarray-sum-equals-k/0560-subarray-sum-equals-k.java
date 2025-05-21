986dfdf3d2f8f6120622be10f71172567fd61117
Suggala Lohith <lohithsuggala@gmail.com>
2025-05-21 10:53:45 +0530

    Time: 28 ms (29.4%), Space: 47.1 MB (15.32%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
+    public int subarraySum(int[] nums, int k) { 
         HashMap<Integer, Integer> subNum = new HashMap<>();
         subNum.put(0, 1);
         int total = 0, count = 0;
